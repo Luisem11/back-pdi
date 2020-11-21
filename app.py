bf18f30aeba5975765ea96c3164b42d5256ca9ee
explora <luis.monsalve@parqueexplora.org>
2020-11-21 14:57:47 -0500

    d

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ app = Flask(__name__)
 def home():
     response = make_response(jsonify(name="API de procesamiento de imágenes de algas", version="1.0"), 200)
     return response
-@app.route('/ping')
-def pong():
-    return 'pong'
+
+@app.route('/prueba', methods=['POST'])
+def p():
+
+    response = make_response(jsonify(param=request.form['par'], version="1.0"), 200)
+    return response
+    
 
 if __name__== '__main__':
     app.run(debug=True, port=4000)
