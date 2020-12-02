c7adb924ed85d35c2d304b46343462fd87628b99
Luis Monsalve <ernesto.monsalve@udea.edu.co>
2020-12-01 21:57:44 -0500

    Intento de conversióna  base 64

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, jsonify, make_response, request
 import os
+import base64
+import numpy as np
+import cv2 
+from io import StringIO
 
 app = Flask(__name__)
 
@@ -10,16 +14,24 @@ def home():
 
 @app.route('/process', methods=['POST'])
 def process():
-    img =  request.json['img']
+    img = readB64(request.json['img'])
+    auximg = img
+    auximg[:,:,1] = 0    
+    auximg[:,:,2] = 0    
+    img = toB64(auximg)
     response = make_response(jsonify(process_img=img), 200)
     return response
     
     
-def readb64(base64_string):
-    sbuf = StringIO()
-    sbuf.write(base64.b64decode(base64_string))
-    pimg = Image.open(sbuf)
-    return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+def readB64(base64_string):    
+    im_bytes = base64.b64decode(base64_string)
+    im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
+    return cv2.imdecode(im_arr, flags=cv2.COLOR_RGB2BGR)
+
+def toB64(img):
+    _, im_arr = cv2.imencode('.jpg', img)  # im_arr: image in Numpy one-dim array format.
+    im_bytes = im_arr.tobytes()
+    im_b64 = base64.b64encode(im_bytes)
 
 if __name__== '__main__':
     app.run(debug=True, port=4000)
